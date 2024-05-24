4b0cd2603ace40369b954318b29f7c4b6648a9fe
terrarian1234 <164236579+terrarian1234@users.noreply.github.com>
2024-05-24 19:59:49 +0400

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+ let myage  = "14"
+ let myname = "luka"
+ let myhobbi = "gaming"
+ let yearofbirth  = "2010"
+ console.log("My name is "+myname )
+ console.log("My age is "+myage +yearofbirth)
+ console.log("My hobbi is "+myhobbi )
+ 
